d00456fad386edc32458ae38e1d3407101a089c5
jojo3420 <jjjhhhvvv@gmail.com>
2020-06-21 17:48:18 +0900

    axios request test code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+  // console.log(process.env.REACT_APP_NEWS_API_KEY);
+
+  const fetch = useCallback(async () => {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    axios
+      .get(url)
+      .then((response) => setData(response.data))
+      .catch((err) => console.error(err));
+
+    // try {
+    //   const result = await axios.get(url);
+    //   console.log({ result });
+    //   setData(result.data);
+    // } catch (e) {
+    //   console.error(e);
+    // }
+  }, []);
+
   return (
     <div>
       <h1>news viewer</h1>
+      <button onClick={fetch}>fetch api test</button>
+      <textarea
+        value={JSON.stringify(data, null, 2)}
+        rows={10}
+        readOnly={true}
+      />
     </div>
   );
 }
